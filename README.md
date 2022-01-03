0c22f057ae086dc7d027bfda50cdba797d89ebf2
zeyadhassan <64292515+zeyadhassan@users.noreply.github.com>
2022-01-03 21:27:19 +0100

    landing page files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# landing_page
+This is a project created for udacity nanodegree program for front-end web development by Zeyad Hassan
+
+It's a basic landing page that uses Javascript to create a NavBar dynamically and to highlight sections when scrolled.
+
+ The project uses HTML, CSS, and Javascript.
---
+++ js/app.js
@@ -0,0 +1,127 @@
+
+/**
+ * Define Global Variables
+ *
+ */
+
+const mybutton = document.getElementById('myBtn');
+const sectionsElements = document.querySelectorAll('section');
+const navbarUl = document.getElementById('navbar__list');
+
+// change title's text
+
+const title = document.getElementById('landing-title');
+
+let navList = '';
+title.textContent = `Udactiy's Project`;
+/**
+ * End Global Variables
+ 
+
+ * Start Helper Functions
+ 
+ *
+ */
+// Generate navbar from sections id names we got from the querySelectorAll
+
+function gernerateNavbar() {
+  sectionsElements.forEach((section) => {
+    // add html tags for list items
+    // dataset.nav returns DOMStringMap {nav: section 1}
+    navList += `<li> <a class="nav__link menu__link" href="#${section.id}" id="navli">
+          ${section.dataset.nav}</a></li>`;
+  });
+  // add the tags to the inner htmls
+  navbarUl.innerHTML = navList;
+}
+gernerateNavbar();
+
+// Add class 'active' to section when near top of viewport (Eye level )
+
+function addActiveClass(section) {
+  // get the id from the section
+  const id = section.getAttribute('id');
+
+  // add the active class to the section
+  document.querySelector(`#${id}`).classList.add('your-active-class');
+}
+
+//Removing the active class from the section
+
+function removeActiveClass(section) {
+  const id = section.getAttribute('id');
+  document.querySelector(`#${id}`).classList.remove('your-active-class');
+}
+
+// calcualting when the section is active
+
+function makeActiveSection() {
+  sectionsElements.forEach((section) => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+    // DOMRect object providing information about the size of an element and its position relative to the viewport.
+    // viewport : A viewport represents a polygonal (normally rectangular) area in computer graphics that is currently being viewed.
+
+    let elementOffset = section.getBoundingClientRect();
+    if (elementOffset.top <= 150 && elementOffset.bottom >= 150) {
+      addActiveClass(section);
+    } else {
+      removeActiveClass(section);
+    }
+  });
+}
+
+// event listener to the dom itself so
+
+document.addEventListener('scroll', makeActiveSection);
+
+// get a button to scroll back to top of the page
+//https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// When the user scrolls down 20px from the top of the document, show the button
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+mybutton.addEventListener('click', topFunction);
+
+let navbar = document.getElementById('navbar').querySelectorAll('li');
+
+// itrate in li items list
+
+navbar.forEach((item) => {
+  item.addEventListener('click', function (e) {
+    navbar.forEach((item) => {
+      // remove every navbarclick class added befoe in any list item
+      item.classList.remove('navbarclick');
+    });
+    // add the class on the button
+    item.classList.add('navbarclick');
+  });
+});
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+
+// Set sections as active
